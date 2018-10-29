c231e0e8250781ecc55a8ce9cb3b79d28669b194
aocny <aocny@aol.com>
2018-10-28 21:55:51 -0400

    worse before better?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
 
+    // var score;
+    // score = 0;
+    // alert("Welcome to the Movie Quiz!")
+    // var q1 = prompt("trivia")
+    // if (q1 === correctAnswer) {
+    //     score++
+    // }
+
+    for(var i=0; i<trivia.length; i++){
+        var response = window.prompt (trivia[i].prompt);
+        if (response === trivia [i].correctAnswer){
+            score++;
+            alert ("Correct!");
+        }else{
+            alert ("Incorrect!");
+        }
+
+    }
+
+    alert("you got" + score)
 
     //Timer 
     var countDown = 30;
@@ -17,41 +37,87 @@ $(document).ready(function () {
   });
 
 
+// An array of quiz questions
 
-
-
-   
-
-
-// We are given an array that holds all of the drinks available
-    var drinkList = [
-      "Coffee: $5",
-      "Espresso: $7",
-      "Cappuccino: $6",
-      "Latte: $4",
-      "Tea: $3",
-      "Ice Coffee: $6",
-      "Ice Espresso: $8",
-      "Ice Latte: $6",
-      "Ice Tea: $4"
-    ];
-
+var trivia = [
+    {
+      question: 'Which of the following actors played James Bond in the most films?',
+      choices: {
+        a: 'Sean Connery',
+        b: 'Roger Moore',
+        c: 'Pierce Brosnan',
+        d: 'Daniel Craig',
+      },
+      correctAnswer: 'b'
+    },
+    {
+      question: 'Which of the following is the top grossing Mission Impossible film?',
+      choices: {
+        a: 'Mission: Impossible III',
+        b: 'Mission: Impossible II',
+        c: 'Mission: Impossible - Fallout',
+        d: 'Mission: Impossile - Ghost Protocol',
+      },
+      correctAnswer: 'c'
+    },
+    {
+      question: 'Which actor has won the most Oscars?',
+      choices: {
+        a: 'Jack Nicholson',
+        b: 'Meryl Streep',
+        c: 'Katherine Hepburn',
+        d: 'Daniel Day-Lewis',
+      },
+      correctAnswer: 'c'
+    },
+    {
+      question: 'Which of the following is the top grossing comedy movie?',
+      choices: {
+        a: 'Home Alone',
+        b: 'Bruce Almighty',
+        c: 'Meet the Fockers',
+        d: 'The Hangover',
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: 'Which comedian was paid the most in 2017?',
+      choices: {
+        a: 'Jeff Dunham',
+        b: 'Terry Fator',
+        c: 'Sebastian Maniscalco',
+        d: 'Jerry Seinfeld',
+      },
+      correctAnswer: 'd'
+    },
+    {
+      question: 'Which female actor was paid the most in 2017?',
+      choices: {
+        a: 'Emma Stone',
+        b: 'Melissa McCarthy',
+        c: 'Jennifer Aniston',
+        d: 'Jennifer Lawrence',
+      },
+      correctAnswer: 'a'
+    }
+  ];
     
 
-    // This line of JavaScript "grabs" the main div on the page ("#drink-options");
-    var drinkDiv = document.getElementById("drink-options");
 
-    // We then use .forEach() to iterate through all the drinks in drinkList.
-    // With each iteration, we perform a series of code operations each time.
-    drinkList.forEach(function(drink) {
+    // // This line of JavaScript "grabs" the main div on the page ("#drink-options");
+    // var drinkDiv = document.getElementById("drink-options");
 
-      // For each drink in the array, we create a new paragraph to hold that text.
-      // A new paragraph will be created with each iteration of the loop.
-      var newDrinkP = document.createElement("p");
+    // // We then use .forEach() to iterate through all the drinks in drinkList.
+    // // With each iteration, we perform a series of code operations each time.
+    // drinkList.forEach(function(drink) {
 
-      // We then assign the the text of this paragraph to be the text in the array item.
-      newDrinkP.textContent = drink;
+    //   // For each drink in the array, we create a new paragraph to hold that text.
+    //   // A new paragraph will be created with each iteration of the loop.
+    //   var newDrinkP = document.createElement("p");
 
-      // We then add the paragraph to the our main div on the page ("#drink-options")
-      drinkDiv.appendChild(newDrinkP);
-    });
+    //   // We then assign the the text of this paragraph to be the text in the array item.
+    //   newDrinkP.textContent = drink;
+
+    //   // We then add the paragraph to the our main div on the page ("#drink-options")
+    //   drinkDiv.appendChild(newDrinkP);
+    // });
